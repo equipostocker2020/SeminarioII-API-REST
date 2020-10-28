ea4c23035ab9dba624e7b0146a190df0de222732
Gonzalo FIgueras <equipostocker2020@gmail.com>
2020-10-28 11:05:03 -0300

    pasando a tipeUser

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,6 @@ const SELECT_BY_ID = 'SELECT * FROM `usuario` WHERE id_usuario = "';
 const INSERT = 'INSERT INTO `usuario` SET ?';
 const DELETE = 'DELETE FROM `usuario` WHERE id_usuario= ?';
 const UPDATE = 'UPDATE `usuario` SET ? WHERE id_usuario = "';
-const SELECT_INSCRIPCIONES = 'SELECT A.id_usuario, A.nombre, A.apellido, B.nombre_materia, B.dia, B.horario, C.anho, D.id_inscripcion, E.nombre_aula from usuario as A  inner join materia as B  inner join aulas_materias as C on c.id_materia = B.id_materia inner join inscripcion as D on D.id_alumno = A.id_usuario and D.id_aula_materia = c.id_rel inner join aula as E on E.id_aula = C.Id_aula where A.rol = "ESTUDIANTE" and A.id_usuario = "'
-const SELECT_ASIGNACIONES_DOCENTE = 'Select A.nombre, A.apellido, B.nombre_materia, B.dia, B.horario, C.anho, E.nombre_aula from usuario as A inner join materia as B inner join aulas_materias as C on c.id_materia = B.id_materia inner join aula as E on E.id_aula = C.Id_aula where A.rol = "DOCENTE" and A.id_usuario = "'
 
 router.get('/', (req, res) => {
     mysqlConnection.query(SELECT, (err, rows) => {
@@ -43,46 +41,6 @@ router.get('/:id', mdAutenticacion.verificaToken, (req, res) => {
                 res.status(200).json({
                     ok: true,
                     usuario: rows
-                });
-            }
-    });
-});
-
-router.get('/alumno/inscripciones/:id', mdAutenticacion.verificaToken, (req, res) => {
-    var id = req.params.id;
-    var sql = SELECT_INSCRIPCIONES + id + '"';
-
-    mysqlConnection.query(sql, [id], (err, rows) => {
-        if (!err)
-            if (rows == 0) {
-                res.status(400).json({
-                    ok: false,
-                    error: 'No existen registros para este ID: ' + id
-                });
-            } else {
-                res.status(200).json({
-                    ok: true,
-                    inscripciones: rows
-                });
-            }
-    });
-});
-
-router.get('/docente/aulas_materias/:id', mdAutenticacion.verificaToken, (req, res) => {
-    var id = req.params.id;
-    var sql = SELECT_ASIGNACIONES_DOCENTE + id + '"';
-
-    mysqlConnection.query(sql, [id], (err, rows) => {
-        if (!err)
-            if (rows == 0) {
-                res.status(400).json({
-                    ok: false,
-                    error: 'No existen registros para este ID: ' + id
-                });
-            } else {
-                res.status(200).json({
-                    ok: true,
-                    aulas_materias: rows
                 });
             }
     });
