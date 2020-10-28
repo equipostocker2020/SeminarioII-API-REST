b2631fff445decb61f3794762dcf9bb9a02dff20
marcelo <equipostocker2020@gmail.com>
2020-10-28 15:34:09 -0300

    manejo de error en nuevas rutas implementadas mfgutierrez

--- routes/tipeUser.js
+++ routes/tipeUser.js
@@ -5,8 +5,8 @@ var mdAutenticacion = require("../middlewares/autentication");
 
 const SELECT_ALUMNO = 'SELECT * FROM `usuario` WHERE usuario.rol = "estudiante"';
 const SELECT_DOCENTE = 'SELECT * FROM `usuario` WHERE usuario.rol = "docente"';
-const SELECT_INSCRIPCIONES = 'SELECT A.id_usuario, A.nombre, A.apellido, B.nombre_materia, B.dia, B.horario, C.anho, D.id_inscripcion, E.nombre_aula from usuario as A  inner join materia as B  inner join aulas_materias as C on c.id_materia = B.id_materia inner join inscripcion as D on D.id_alumno = A.id_usuario and D.id_aula_materia = c.id_rel inner join aula as E on E.id_aula = C.Id_aula where A.rol = "ESTUDIANTE" and A.id_usuario = "'
-const SELECT_ASIGNACIONES_DOCENTE = 'Select A.nombre, A.apellido, B.nombre_materia, B.dia, B.horario, C.anho, E.nombre_aula from usuario as A inner join materia as B inner join aulas_materias as C on c.id_materia = B.id_materia inner join aula as E on E.id_aula = C.Id_aula where A.rol = "DOCENTE" and A.id_usuario = "'
+const SELECT_INSCRIPCIONES = 'SELECT A.id_usuario, A.nombre, A.apellido, B.nombre_materia, B.dia, B.horario, C.anho, D.id_inscripcion, E.nombre_aula from usuario as A  inner join materia as B  inner join aulas_materias as C on C.id_materia = B.id_materia inner join inscripcion as D on D.id_alumno = A.id_usuario and D.id_aula_materia = C.id_rel inner join aula as E on E.id_aula = C.Id_aula where A.rol = "ESTUDIANTE" and A.id_usuario = "'
+const SELECT_ASIGNACIONES_DOCENTE = 'Select A.nombre, A.apellido, B.nombre_materia, B.dia, B.horario, C.anho, E.nombre_aula from usuario as A inner join materia as B inner join aulas_materias as C on C.id_materia = B.id_materia inner join aula as E on E.id_aula = C.Id_aula where A.rol = "DOCENTE" and A.id_usuario = "'
 
 router.get("/alumno", mdAutenticacion.verificaToken, (req, res) => {
     var sql = SELECT_ALUMNO;
@@ -51,8 +51,15 @@ router.get("/docente", mdAutenticacion.verificaToken, (req, res) => {
 router.get('/alumno/inscripciones/:id', mdAutenticacion.verificaToken, (req, res) => {
     var id = req.params.id;
     var sql = SELECT_INSCRIPCIONES + id + '"';
+    console.log(sql);
 
     mysqlConnection.query(sql, [id], (err, rows) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                error: err
+            });
+        }
         if (!err)
             if (rows == 0) {
                 res.status(400).json({
@@ -73,6 +80,12 @@ router.get('/docente/aulas_materias/:id', mdAutenticacion.verificaToken, (req, r
     var sql = SELECT_ASIGNACIONES_DOCENTE + id + '"';
 
     mysqlConnection.query(sql, [id], (err, rows) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                error: err
+            });
+        }
         if (!err)
             if (rows == 0) {
                 res.status(400).json({
