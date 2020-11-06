7e47c63c72ab7903d819571836063b82ba5fd524
Gonzalo FIgueras <equipostocker2020@gmail.com>
2020-11-06 13:27:20 -0300

    select notasxalumno vista docente

--- routes/tipeUser.js
+++ routes/tipeUser.js
@@ -9,6 +9,7 @@ const SELECT_INSCRIPCIONES = 'SELECT A.id_usuario, A.nombre, A.apellido, B.nombr
 const SELECT_ASIGNACIONES_DOCENTE = 'select A.id_rel,A.anho,B.id_materia,B.nombre_materia,B.dia, B.horario, E.id_usuario, F.id_aula,F.nombre_aula from aulas_materias as A inner join materia as B on B.id_materia = A.id_materia inner join usuario as E on E.id_usuario = A.id_docente inner join aula as F on F.id_aula = A.id_aula where A.id_docente = "';
 const SELECT_BY_ID_AULA_MATERIA = 'SELECT A.id_inscripcion, B.id_rel, B.id_docente, B.anho, C.id_aula, C.nombre_aula, D.id_materia, D.nombre_materia, D.dia, D.horario, E.id_usuario, E.nombre, E.apellido, E.dni, E.email FROM inscripcion AS A INNER JOIN aulas_materias AS B ON B.id_rel = A.id_aula_materia INNER JOIN aula AS C ON C.id_aula = B.Id_aula INNER JOIN materia AS D ON D.id_materia = B.id_materia INNER JOIN usuario AS E ON E.id_usuario = A.id_alumno WHERE B.id_rel = "';
 const SELECT_MIS_NOTAS = 'SELECT a.id_nota,a.nota, b.id_instancia, b.nombre_instancia, c.id_inscripcion, c.id_alumno, d.id_usuario,e.id_rel,f.id_materia,f.nombre_materia from nota_alumno as a inner join instancia_evaluacion as b on b.id_instancia = a.id_instancia inner join inscripcion as c on c.id_inscripcion = a.id_inscripcion inner join usuario as d on d.id_usuario = c.id_alumno inner join aulas_materias as e on e.id_rel = c.id_aula_materia inner join materia as f on f.id_materia = e.id_materia WHERE c.id_alumno = "';
+const SELECT_DOCENTE_NOTASXALUMNO = 'SELECT a.id_nota,a.nota, b.id_instancia, b.nombre_instancia, c.id_inscripcion, c.id_alumno, d.id_usuario,e.id_rel, c.id_aula_materia, f.id_materia,f.nombre_materia,e.id_docente from nota_alumno as a inner join instancia_evaluacion as b on b.id_instancia = a.id_instancia inner join inscripcion as c on c.id_inscripcion = a.id_inscripcion inner join usuario as d on d.id_usuario = c.id_alumno inner join aulas_materias as e on e.id_rel = c.id_aula_materia inner join materia as f on f.id_materia = e.id_materia inner join usuario as g on g.id_usuario = e.id_docente WHERE c.id_alumno = "';
 const SELECT_DOCENTE_ID = 'SELECT * from `usuario` WHERE rol = "DOCENTE" AND id_usuario = "';
 const SELECT_ALUMNOS_ID = 'SELECT * from `usuario` WHERE rol = "ESTUDIANTE" AND id_usuario = "';
 
@@ -212,6 +213,33 @@ router.get('/alumno/misnotas/:id', mdAutenticacion.verificaToken, (req, res) => 
                 res.status(400).json({
                     ok: false,
                     error: 'No existen registros para este ID: ' + id
+                });
+            } else {
+                res.status(200).json({
+                    ok: true,
+                    inscripciones: rows
+                });
+            }
+    });
+});
+
+router.get('/docente/notasxalumno/:id_alumno', mdAutenticacion.verificaToken, (req, res) => {
+    var id_alumno = req.params.id_alumno;
+    var id_docente = req.query.id_usuario;
+    var sql = SELECT_DOCENTE_NOTASXALUMNO + id_alumno + '"' + ' and e.id_docente = "' + id_docente + '"';
+    console.log(sql);
+    mysqlConnection.query(sql, [id_alumno], (err, rows) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                error: err
+            });
+        }
+        if (!err)
+            if (rows == 0) {
+                res.status(400).json({
+                    ok: false,
+                    error: 'No existen registros para este ID: ' + id_alumno
                 });
             } else {
                 res.status(200).json({
